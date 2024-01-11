5c6eaaee9365844c2d6d203e68b57a46bff08268
ZakyDianto <140376583+ZakyDianto@users.noreply.github.com>
2024-01-11 14:53:31 +0700

    Add files via upload

---
+++ UjiBus.java
@@ -0,0 +1,21 @@
+package Latihan2;
+
+public class UjiBus {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bus busMini=new Bus(10);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(3);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+          
+    }
+
+}
